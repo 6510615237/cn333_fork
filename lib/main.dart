b5d871707a6a117aae9cb6a66a9960c51998865e
6510615138 <r.nakinton@gmail.com>
2025-02-28 16:50:45 +0700

    merge front end to dev

---
+++ lib/main.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'dart:async'; 
+import 'package:google_fonts/google_fonts.dart'; 
+import 'screens/login.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_option.dart';
+import 'pages/homepage.dart';
+import 'pages/login.dart';
+
+
+ThemeData lightMode = ThemeData(
+  textTheme: const TextTheme(
+  bodyLarge: TextStyle(
+    color: Color.fromARGB(255, 255, 255, 255),
+  ), // Default text color for large text
+  bodyMedium: TextStyle(
+    color: Color.fromARGB(255, 255, 255, 255),
+  ), // Default text color for medium text
+  bodySmall: TextStyle(
+    color: Color.fromARGB(255, 255, 255, 255),
+  ), // Default text color for small text
+ ),
+  colorScheme: ColorScheme.light(
+    surface: Colors.grey.shade50,
+    primary: Colors.grey.shade100,
+    secondary: Colors.grey.shade200,
+    tertiary: Colors.grey.shade400,
+    inversePrimary: Colors.grey.shade600
+
+  )
+);
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+<<<<<<< HEAD:tuquest/lib/main.dart
+=======
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TU Quest',
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.hasData) {
+            return const HomePage();
+          } else {
+            return const LoginPage();
+          }
+        },
+      ),
+      theme: lightMode,
+
+  // This widget is the root of your application.
+>>>>>>> main:lib/main.dart
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      title: 'TUQuest',
+
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 166, 35, 39),
+        ),
+      ),
+<<<<<<< HEAD:tuquest/lib/main.dart
+      home: const SplashPage(), 
+=======
+
+      home: const MyHomePage(title: 'TUQuest'),
+
+>>>>>>> main:lib/main.dart
+    );
+  }
+}
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => SplashPageState();
+}
+
+class SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    // เปลี่ยนไปหน้า login หลัง 5 วิ
+    Timer(const Duration(seconds: 5), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginPage()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xFF000000),
+              Color(0xFFFF0004),
+            ],
+          ),
+        ),
+        child: Center(
+          child: Text(
+            "TUQuest",
+            style: GoogleFonts.montserrat(
+              fontSize: 48,
+              fontWeight: FontWeight.w800, // ExtraBold
+              foreground: Paint()
+                ..shader = const LinearGradient(
+                  colors: [
+                    Color(0xFFA00000),
+                    Color(0xFFEA2520),
+                    Color(0xFFFF8000),
+                  ],
+                ).createShader(const Rect.fromLTWH(0, 0, 200, 50)),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
